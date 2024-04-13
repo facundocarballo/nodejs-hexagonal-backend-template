4da239bba83a8eb52553b92823b2a19a36e2444f
Facundo Carballo <carballofacundo70@gmail.com>
2024-04-13 20:32:05 -0300

    refactor: remove unused file import

--- test/e2e/create-user.spec.ts
+++ test/e2e/create-user.spec.ts
@@ -4,7 +4,6 @@ import request from "supertest";
 import http from "http";
 import { CreateUserUseCaseInput } from "#app/use-cases/create-user/create-user-input";
 import { CreateUserUserCaseInputMother } from "test/app/use-cases/create-user/create-user-input";
-import { UserCantCreate } from "#app/exceptions/user-cant-create";
 
 describe("E2E - CREATE - '/user'", () => {
   let app: Application;
