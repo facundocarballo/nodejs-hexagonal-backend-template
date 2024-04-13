da95a1a9acb09f98728aea431e6f461d165379ed
Facundo Carballo <carballofacundo70@gmail.com>
2024-04-13 20:02:02 -0300

    feat: get user use case tested

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    modulePaths: ['<rootDir>'],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
+};
---
+++ test/mothers/constants.ts
@@ -0,0 +1,5 @@
+export class ConstantsMother {
+  static readonly USER_ID = "1234";
+  static readonly USERNAME = "facundocarballo";
+  static readonly PASSWORD = "123456789";
+}
