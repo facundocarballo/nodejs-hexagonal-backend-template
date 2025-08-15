cc3872d2d5e38847646e64e633f4ea20e0ccfb93
Facundo Carballo <59286028+facundocarballo@users.noreply.github.com>
2025-08-15 12:38:28 -0300

    Dockerfile and script to build (#6)
    * feat: dockerfile-and-script-to-build
    
    * fix: tests

--- test/e2e/create-user.spec.ts
+++ test/e2e/create-user.spec.ts
@@ -1,6 +1,5 @@
 import { Application } from "express";
 import request from "supertest";
-import http from "http";
 import { CreateUserUseCaseInput } from "#app/use-cases/create-user/create-user-input";
 import { CreateUserUserCaseInputMother } from "test/app/use-cases/create-user/create-user-input";
 import { StatusCodes } from "http-status-codes";
@@ -8,23 +7,13 @@ import { bootstrapApp } from "#bootstraps/app";
 
 describe("E2E - CREATE - '/user'", () => {
   let app: Application;
-  let server: http.Server;
 
   beforeAll(() => {
     try {
       app = bootstrapApp();
-      server = app.listen(8080);
     } catch (error) {
       console.error("Error creating the server: ", error);
       process.exit(1);
-    }
-  });
-
-  afterAll(() => {
-    try {
-      server.close();
-    } catch (error) {
-      console.error("Error closing the server: ", error);
     }
   });
 
--- test/e2e/get-user.spec.ts
+++ test/e2e/get-user.spec.ts
@@ -1,28 +1,17 @@
 import { Application } from "express";
 import { StatusCodes } from "http-status-codes";
 import request from "supertest";
-import http from "http";
 import { bootstrapApp } from "#bootstraps/app";
 
 describe("E2E - GET - '/user'", () => {
   let app: Application;
-  let server: http.Server;
 
   beforeAll(() => {
     try {
       app = bootstrapApp();
-      server = app.listen(8080);
     } catch (error) {
       console.error("Error creating the server: ", error);
       process.exit(1);
-    }
-  });
-
-  afterAll(() => {
-    try {
-      server.close();
-    } catch (error) {
-      console.error("Error closing the server: ", error);
     }
   });
 
