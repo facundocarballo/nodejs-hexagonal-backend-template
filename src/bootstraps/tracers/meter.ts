7d7c33a1ba16fe8a6d4da3bc52783219caa88956
facundocarballo <carballofacundo70@gmail.com>
2026-01-09 11:21:45 -0300

    refactor: metric-name

--- src/bootstraps/tracers/meter.ts
+++ src/bootstraps/tracers/meter.ts
@@ -1,7 +1,7 @@
 import { metrics } from '@opentelemetry/api';
-
+import packageJson from '../../../package.json'
 export class BootstrapMeter {
     public static get() {
-        return metrics.getMeter('name_ms');
+        return metrics.getMeter(packageJson.name);
     }
 }
